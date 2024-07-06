877614b68ea5905bfd9027c707539282ea74682b
Agusamr <m.agustina.amore@gmail.com>
2024-07-06 19:58:50 +0000

    noveno-commit

--- backend/rest_api_pizzas/settings.py
+++ backend/rest_api_pizzas/settings.py
@@ -84,13 +84,22 @@ WSGI_APPLICATION = "rest_api_pizzas.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    "default" :{
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "pizzas_db",
+        "USER": "root",
+        "PASSWORD": "codo_codo",
+        "HOST": "127.0.0.1",
+        "PORT": "3306",
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
